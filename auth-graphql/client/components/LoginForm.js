3c76479d34a5435a3ab8a482a1a24246ba649c1e
Andres <jose.vargas@prodigious.cr>
2018-07-06 16:15:07 -0600

    Login component

---
+++ auth-graphql/client/components/LoginForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
+
+class LoginForm extends Component {
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }]
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(err => err.message);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(LoginForm);
